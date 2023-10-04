aa0951151385a2c9cc87cdaa140a6e7087e90f4c
Karthikeya Narla <narla.karthikeya@iitgn.ac.in>
2023-10-04 18:24:11 +0530

    test successful

--- src/main/java/com/iitgn/entryexit/config/SecurityConfig.java
+++ src/main/java/com/iitgn/entryexit/config/SecurityConfig.java
@@ -1,18 +1,13 @@
 package com.iitgn.entryexit.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig{
 
     private UserDetailsService userDetailsService;
 
--- src/main/java/com/iitgn/entryexit/services/CustomUserDetailsService.java
+++ src/main/java/com/iitgn/entryexit/services/CustomUserDetailsService.java
@@ -1,22 +1,24 @@
 package com.iitgn.entryexit.services;
 
 import com.iitgn.entryexit.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
-@RequiredArgsConstructor
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(usernameOrEmail)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found with email: "+ username));
+                        new UsernameNotFoundException("User not found with username or email: "+ usernameOrEmail));
     }
 }
